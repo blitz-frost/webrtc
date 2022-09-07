2c8e41604e832e62ada1966ccad34b9d449fffa4
Bogdan <mereu.etern@gmail.com>
2022-09-08 01:07:05 +0300

    zero

---
+++ webrtc.go
@@ -0,0 +1,216 @@
+package webrtc
+
+import (
+	"sync"
+
+	"github.com/blitz-frost/encoding/json"
+	msgenc "github.com/blitz-frost/encoding/msg"
+	"github.com/blitz-frost/io"
+	msgio "github.com/blitz-frost/io/msg"
+	"github.com/blitz-frost/msg"
+	"github.com/blitz-frost/rpc"
+	"github.com/pion/webrtc/v3"
+)
+
+type Channel struct {
+	V *webrtc.DataChannel
+
+	buf []byte // buffer outgoing messages
+	dst msgio.ReaderTaker
+}
+
+// NewChannel wraps a [webrtc.DataChannel] to fit the msg framework.
+func NewChannel(v *webrtc.DataChannel) *Channel {
+	x := Channel{
+		V:   v,
+		buf: []byte{},
+		dst: msgio.Void{},
+	}
+	v.OnMessage(func(m webrtc.DataChannelMessage) {
+		x.dst.ReaderTake((*io.BytesReader)(&m.Data))
+	})
+	return &x
+}
+
+func (x *Channel) Close() error {
+	return x.V.Close()
+}
+
+func (x *Channel) OnClose(fn func()) {
+	x.V.OnClose(fn)
+}
+
+/*
+Not present in wasm version
+func (x Channel) OnError(fn func(error)) {
+	x.ch.OnError(fn)
+}
+*/
+
+func (x *Channel) OnOpen(fn func()) {
+	x.V.OnOpen(fn)
+}
+
+func (x *Channel) ReaderChain(dst msgio.ReaderTaker) error {
+	x.dst = dst
+	return nil
+}
+
+// The returned value is also a [msg.Canceler].
+// Not concurrent safe.
+func (x *Channel) Writer() (msgio.Writer, error) {
+	return (*writer)(x), nil
+}
+
+type signaler struct {
+	fnCandidate func(string) error
+	fnSdp       func(webrtc.SessionDescription) error
+}
+
+func (x *signaler) candidate(candidate *webrtc.ICECandidate) error {
+	arg := candidate.ToJSON().Candidate
+	return x.fnCandidate(arg)
+}
+
+func (x *signaler) setup(conn *webrtc.PeerConnection, c msgio.Conn, rCh, wCh byte, answerFunc func() error) error {
+	// ensure concurrency
+	rc, err := msgio.NewReaderChainerAsync(c)
+	if err != nil {
+		return err
+	}
+	wg := msgio.NewWriterGiverMutex(c)
+	block := msg.ConnBlock[msgio.Reader, msgio.Writer]{rc, wg}
+
+	// form ExchangeConn
+	mc, err := msgio.MultiplexConnOf(block)
+	if err != nil {
+		return err
+	}
+	rConn := msgio.ConnOf(mc, rCh)
+	wConn := msgio.ConnOf(mc, wCh)
+
+	ec, err := msgio.ExchangeConnOf(rConn, wConn)
+	if err != nil {
+		return err
+	}
+	ecEnc, err := msgenc.ExchangeConnOf(ec, json.Codec)
+	if err != nil {
+		return err
+	}
+
+	pending := make([]*webrtc.ICECandidate, 0)
+	mux := sync.Mutex{}
+
+	// prepare rpc answer side
+	lib := rpc.MakeLibrary()
+	lib.Register("candidate", func(s string) error {
+		return conn.AddICECandidate(webrtc.ICECandidateInit{Candidate: s})
+	})
+	lib.Register("sdp", func(sdp webrtc.SessionDescription) error {
+		if err := conn.SetRemoteDescription(sdp); err != nil {
+			return err
+		}
+
+		if err := answerFunc(); err != nil {
+			return err
+		}
+
+		mux.Lock()
+		for _, candidate := range pending {
+			go x.candidate(candidate)
+		}
+		mux.Unlock()
+
+		return nil
+	})
+
+	answerGate := rpc.MakeAnswerGate(lib)
+	if err := ecEnc.ReaderChain(answerGate); err != nil {
+		return err
+	}
+
+	// prepare rpc call side
+	callGate := rpc.MakeCallGate(ecEnc)
+	cli := rpc.MakeClient(callGate)
+
+	cli.Bind("candidate", &x.fnCandidate)
+	cli.Bind("sdp", &x.fnSdp)
+
+	conn.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+		if candidate == nil {
+			return
+		}
+
+		mux.Lock()
+		defer mux.Unlock()
+
+		desc := conn.RemoteDescription()
+		if desc == nil {
+			pending = append(pending, candidate)
+		} else {
+			go x.candidate(candidate)
+		}
+	})
+
+	return nil
+}
+
+// SignalAnswer sets up the WebRTC answer side of the signaling process for a peer connection, using the provided data carrier.
+func SignalAnswer(conn *webrtc.PeerConnection, c msgio.Conn) error {
+	sig := signaler{}
+	answerFunc := func() error {
+		answer, err := conn.CreateAnswer(nil)
+		if err != nil {
+			return err
+		}
+
+		go sig.fnSdp(answer)
+
+		return conn.SetLocalDescription(answer)
+	}
+
+	return sig.setup(conn, c, 1, 0, answerFunc)
+}
+
+// SignalOffer sets up the WebRTC offer side of the signaling process for a peer connection, using the provided data carrier.
+// The returned function can be used to start the initial process, as well as renegotiation.
+func SignalOffer(conn *webrtc.PeerConnection, c msgio.Conn) (func() error, error) {
+	sig := signaler{}
+	answerFunc := func() error { return nil }
+
+	if err := sig.setup(conn, c, 0, 1, answerFunc); err != nil {
+		return nil, err
+	}
+
+	fn := func() error {
+		offer, err := conn.CreateOffer(nil)
+		if err != nil {
+			return err
+		}
+		if err = conn.SetLocalDescription(offer); err != nil {
+			return err
+		}
+
+		return sig.fnSdp(offer)
+	}
+
+	return fn, nil
+}
+
+type writer Channel
+
+func (x *writer) Cancel() error {
+	x.buf = x.buf[:0]
+	return nil
+}
+
+func (x *writer) Close() error {
+	err := x.V.Send(x.buf)
+	x.buf = x.buf[:0]
+	return err
+}
+
+func (x *writer) Write(b []byte) (int, error) {
+	x.buf = append(x.buf, b...)
+	return len(b), nil
+}
---
+++ webrtc_wasm.go
@@ -0,0 +1,139 @@
+//go:build wasm && js
+
+// Package webrtc wraps the Javascript WebRTC API.
+// Currently mostly just complements the pion/webrtc package.
+package webrtc
+
+import (
+	"syscall/js"
+
+	"github.com/blitz-frost/wasm"
+	"github.com/blitz-frost/wasm/media"
+)
+
+type Conn struct {
+	v js.Value
+
+	onTrack js.Func
+}
+
+func AsConn(v js.Value) *Conn {
+	return &Conn{
+		v: v,
+	}
+}
+
+func (x Conn) AddTrack(track media.Track) (Sender, error) {
+	v, err := wasm.Call(x.v, "addTrack", track.Js())
+	return Sender{v}, err
+}
+
+func (x *Conn) OnTrack(fn func(track media.Track, streams []media.Stream)) {
+	x.onTrack.Release()
+
+	x.onTrack = js.FuncOf(func(this js.Value, args []js.Value) any {
+		track := media.AsTrack(args[0].Get("track"))
+		streamsJs := args[0].Get("streams")
+		var streams []media.Stream
+		for i, n := 0, streamsJs.Length(); i < n; i++ {
+			v := streamsJs.Index(i)
+			streams = append(streams, media.AsStream(v))
+		}
+
+		fn(track, streams)
+		return nil
+	})
+
+	x.v.Set("ontrack", x.onTrack)
+}
+
+func (x Conn) Release() {
+	x.onTrack.Release()
+}
+
+type EncodingParameters struct {
+	v js.Value
+}
+
+func (x EncodingParameters) Active() bool {
+	return x.v.Get("active").Bool()
+}
+
+func (x EncodingParameters) Downscale() float64 {
+	return x.v.Get("scaleResolutionDownBy").Float()
+}
+
+// Only for video tracks.
+// factor must be >= 1 and is applied to both image dimensions.
+func (x EncodingParameters) DownscaleSet(factor float64) {
+	x.v.Set("scaleResolutionDownBy", factor)
+}
+
+func (x EncodingParameters) MaxBitrate() uint {
+	v := x.v.Get("maxBitrate")
+	return uint(v.Int())
+}
+
+func (x EncodingParameters) MaxBitrateSet(br uint) {
+	x.v.Set("maxBitrate", br)
+}
+
+func (x EncodingParameters) MaxFramerate() float64 {
+	return x.v.Get("maxFramerate").Float()
+}
+
+func (x EncodingParameters) MaxFramerateSet(fps float64) {
+	x.v.Set("maxFramerate", fps)
+}
+
+func (x EncodingParameters) PayloadType() byte {
+	v := x.v.Get("codecPayloadType")
+	return byte(v.Int())
+}
+
+func (x EncodingParameters) Ptime() uint {
+	v := x.v.Get("ptime")
+	return uint(v.Int())
+}
+
+func (x EncodingParameters) PtimeSet(ms uint) {
+	x.v.Set("ptime", ms)
+}
+
+func (x EncodingParameters) Rid() string {
+	return x.v.Get("rid").String()
+}
+
+type SendParameters struct {
+	v js.Value
+}
+
+// Modify the return values directly, then call Sender.ParametersSet(x).
+func (x SendParameters) Encodings() []EncodingParameters {
+	encodings := x.v.Get("encodings")
+
+	n := encodings.Length()
+	o := make([]EncodingParameters, n)
+	for i := 0; i < n; i++ {
+		v := encodings.Index(i)
+		o[i] = EncodingParameters{v}
+	}
+
+	return o
+}
+
+type Sender struct {
+	v js.Value
+}
+
+func (x Sender) Parameters() SendParameters {
+	v := x.v.Call("getParameters")
+	return SendParameters{v}
+}
+
+// Must be called with the return value of the last Parameters method call.
+func (x Sender) ParametersSet(params SendParameters) error {
+	promise := x.v.Call("setParameters", params.v)
+	_, err := wasm.Await(promise)
+	return err
+}
